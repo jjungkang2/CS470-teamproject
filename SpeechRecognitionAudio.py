86ad5cbe5dd7328783845ebd5536327f4e212075
jjungkang2 <jjungkang2@kaist.ac.kr>
2020-11-19 02:53:03 +0900

    STT

---
+++ SpeechRecognitionAudio.py
@@ -0,0 +1,21 @@
+"""
+google web speech api
+녹음 파일 STT
+
+초기 세팅
+pip install SpeechRecognition
+"""
+
+import speech_recognition as sr
+
+AUDIO_FILE = "hi.wav"
+
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source) 
+
+# 구글 웹 음성 API로 인식하기 (하루에 제한 50회)
+try:
+    print(r.recognize_google(audio, language='en', show_all=True))
+except Exception as e:
+    print(str)
---
+++ SpeechRecognitionMicrophone.py
@@ -0,0 +1,24 @@
+"""
+google web speech api
+실시간 마이크 STT
+
+초기 세팅
+Visual Studio 설치하고 C++ 패키지 설치
+pip install wheel
+pip install pipwin
+pipwin install pyaudio
+"""
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+    audio = r.listen(source)
+
+# 구글 웹 음성 API로 인식하기 (하루에 제한 50회)
+try:
+    print(r.recognize_google(audio, language='en', show_all=True))
+except Exception as e:
+    print(str)
