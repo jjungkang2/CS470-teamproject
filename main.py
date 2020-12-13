f5a3e4ec47f6cf735916d473648b40fb369da12f
Jinhee Ryu <70677320+jinhee-yoo@users.noreply.github.com>
2020-12-13 23:33:52 +0900

    Update main.py
    add omitted code for executing convert_to_video()

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 from Speech_to_text import make_input_for_model
 from Punctuation_restoration import execute_punctuator
 from DisfluencyRemover.main import execute_disfluency_remover
+from Text_to_sign_language.TextToSignLanguage import convert_to_video
 
 def main():
     # Speech to text
     file_path = os.getcwd() + '/Resources/audio_sample.wav'
     input = make_input_for_model(file_path)
-    print("-----STT result-----")
+    print("-----STT Result-----")
     print(input)
 
     # Punctuation restoration model
@@ -16,14 +17,18 @@ def main():
     f = open(file_path_punctuator, 'w')
     f.write(output)
     f.close()
-    print("-----Punctuator result-----")
+    print("-----Punctuator Result-----")
     print(output)
 
     # Disfluency detection
     os.chdir('DisfluencyRemover')
     execute_disfluency_remover()
-    print("-----Disfluency Remover execution completed-----")
-
+    print("-----Disfluency Remover Execution Completed-----")
+    
+    # Generation of sign language video
+    os.chdir(os.pardir + 'Text_to_sign_language')
+    convert_to_video()
+    print("-----Sign Language Video Generation Completed-----")
 
 if __name__ == '__main__':
     main()
